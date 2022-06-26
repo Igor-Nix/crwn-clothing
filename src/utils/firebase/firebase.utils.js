d9ce82c62a81382fd8c92795544c89c951dbec1a
Igor Nichiforov <70261890+Igor-Nix@users.noreply.github.com>
2022-06-26 13:38:45 +0200

    Adding firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgufk5yoMnldSA57y41HjzGOyt8n6uenI",
+  authDomain: "crwn-clothing-db-f66e9.firebaseapp.com",
+  projectId: "crwn-clothing-db-f66e9",
+  storageBucket: "crwn-clothing-db-f66e9.appspot.com",
+  messagingSenderId: "68972118041",
+  appId: "1:68972118041:web:18e4ee986428c66544aa88",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
